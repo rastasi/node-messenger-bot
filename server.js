abec6da659120e0452a25ef31bee7680ae7dba17
Zsolt Tasnadi <rastasi@gmail.com>
2019-01-24 14:10:21 +0100

    refactoring

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const app = express().use(bodyParser.json());
 const VERIFY_TOKEN = '';
 const PAGE_ACCESS_TOKEN = '';
 
+const getRequestParameters = requestBody => {
+  return {
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { 'access_token': PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: requestBody
+  };
+};
+
 const callSendAPI = (senderPsid, response) => {
   let requestBody = {
     recipient: {
@@ -14,29 +23,19 @@ const callSendAPI = (senderPsid, response) => {
     message: response
   };
 
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { 'access_token': PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: requestBody
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!');
-    } else {
-      console.error('Unable to send message:' + err);
-    }
+  request(getRequestParameters(requestBody), (err, res, body) => {
+    console.log(err ? `Unable to send message: ${err}` : 'Message sent!');
   });
 };
 
-const handlePostback = (senderPsid, receivedPostback) => {
-  let response;
-  let payload = receivedPostback.payload;
+const getResponse = (payload) => {
+  if (payload === 'yes') return { text: 'Thanks!' };
+  return { text: 'Oops, try sending another image.' };
+};
 
-  if (payload === 'yes') {
-    response = { text: 'Thanks!' };
-  } else if (payload === 'no') {
-    response = { text: 'Oops, try sending another image.' };
-  }
+const handlePostback = (senderPsid, receivedPostback) => {
+  const payload = receivedPostback.payload;
+  const response = getResponse(payload);
   callSendAPI(senderPsid, response);
 };
 
@@ -50,41 +49,31 @@ const handleMessage = (senderPsid, receivedMessage) => {
   callSendAPI(senderPsid, response);
 };
 
-app.post('/webhook', (req, res) => {
-  let body = req.body;
+const processEntry = entry => {
+  const webhookEvent = entry.messaging[0];
+  const senderPsid = webhookEvent.sender.id;
 
-  if (body.object === 'page') {
-    body.entry.forEach(function (entry) {
-      let webhookEvent = entry.messaging[0];
-      let senderPsid = webhookEvent.sender.id;
-
-      if (webhookEvent.message) {
-        handleMessage(senderPsid, webhookEvent.message);
-      } else if (webhookEvent.postback) {
-        handlePostback(senderPsid, webhookEvent.postback);
-      }
-    });
-
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    res.sendStatus(404);
+  if (webhookEvent.message) {
+    handleMessage(senderPsid, webhookEvent.message);
+  } else if (webhookEvent.postback) {
+    handlePostback(senderPsid, webhookEvent.postback);
   }
+};
+
+app.post('/webhook', (req, res) => {
+  if (req.body.object !== 'page') return res.sendStatus(404);
+  req.body.entry.forEach(processEntry);
+  res.status(200).send('EVENT_RECEIVED');
 });
 
 app.get('/webhook', (req, res) => {
-
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-
-  if (mode && token) {
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
-  }
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+  if (!(mode && token)) return;
+  if (!(mode === 'subscribe' && token === VERIFY_TOKEN)) return res.sendStatus(403);
+  console.log('WEBHOOK_VERIFIED');
+  res.status(200).send(challenge);
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
+app.listen(process.env.PORT, () => console.log('webhook is listening'));
